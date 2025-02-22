a522f1229649f9629038901ca2d270d36dda7104
Shaxobiddin <Shaxobiddinnormatov2@gmail.com>
2025-02-22 12:56:35 +0300

    static file lar yig'ildi

--- bace/settings.py
+++ bace/settings.py
@@ -151,11 +151,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
 
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Static fayllar saqlanadigan joy
 STATICFILES_DIRS = [
-    BASE_DIR /'static',
+    os.path.join(BASE_DIR, "static"),  # Agar loyihangizda static papka bo‘lsa
 ]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
